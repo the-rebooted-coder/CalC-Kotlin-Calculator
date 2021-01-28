3580d41e6569f32259eceb070b8c988210d586a5
Spandan Saxena <spandansaxena@gmail.com>
2021-01-28 21:50:26 +0530

    Changed Main Activity to Splash Screen

--- app/src/main/java/com/aaxena/calculator/SplashScreen.kt
+++ app/src/main/java/com/aaxena/calculator/SplashScreen.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 
-class MainActivity : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.splash_screen)
         }
 }
