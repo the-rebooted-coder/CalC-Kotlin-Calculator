4be563076d4c574e50454e28ddc11189d5255d00
Spandan Saxena <spandansaxena@gmail.com>
2021-01-28 22:07:30 +0530

    Fixed Depereciated API

--- app/src/main/java/com/aaxena/calculator/SplashScreen.kt
+++ app/src/main/java/com/aaxena/calculator/SplashScreen.kt
@@ -3,19 +3,21 @@ package com.aaxena.calculator
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreen : AppCompatActivity() {
-    private val SPLASH_SCREEN_TIME_OUT:Long = 3000 // 1 sec
+    private val SPLASH_SCREEN_TIME_OUT:Long = 1000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
 
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             // This method will be executed once the timer is over
             // Start your app main activity
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
             finish()
         }, SPLASH_SCREEN_TIME_OUT)
         }
