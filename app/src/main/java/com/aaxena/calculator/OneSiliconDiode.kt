c11d3b1f1c0db55c025e7156ef85b7555fed6368
Spandan Saxena <spandansaxena@gmail.com>
2021-01-29 23:47:15 +0530

    Created OneSIliconDiode Activity

---
+++ app/src/main/java/com/aaxena/calculator/OneSiliconDiode.kt
@@ -0,0 +1,11 @@
+package com.aaxena.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OneSiliconDiode : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_one_silicon_diode)
+    }
+}
