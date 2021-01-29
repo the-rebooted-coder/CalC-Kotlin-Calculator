d3ee262de5e93c5d2c0059c3e55b311f80ff7134
Spandan Saxena <spandansaxena@gmail.com>
2021-01-29 20:04:25 +0530

    Beautifullied

--- app/src/main/java/com/aaxena/calculator/MainActivity.kt
+++ app/src/main/java/com/aaxena/calculator/MainActivity.kt
@@ -1,50 +1,82 @@
 package com.aaxena.calculator
 
-import android.os.Bundle
-import android.view.View
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.fathzer.soft.javaluator.DoubleEvaluator
-
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import net.objecthunter.exp4j.ExpressionBuilder
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-     fun onClick(v: View?) {
-        var tv = v as TextView
-        var resultTV = findViewById<TextView>(R.id.resultTV)
-        var oldText = resultTV.text.toString()
 
-        when(tv.text.toString()){
-            "Del" -> {
-                if(oldText.length > 0){
-                    var newText = oldText.substring(0, oldText.length - 1)
-                    resultTV.setText(newText)
-                }
+
+        //Numbers
+        num0.setOnClickListener { appendVal("0", false) }
+        num1.setOnClickListener { appendVal("1", false) }
+        num2.setOnClickListener { appendVal("2", false) }
+        num3.setOnClickListener { appendVal("3", false) }
+        num4.setOnClickListener { appendVal("4", false) }
+        num5.setOnClickListener { appendVal("5", false) }
+        num6.setOnClickListener { appendVal("6", false) }
+        num7.setOnClickListener { appendVal("7", false) }
+        num8.setOnClickListener { appendVal("8", false) }
+        num9.setOnClickListener { appendVal("9", false) }
+        numDot.setOnClickListener { appendVal(".", false) }
+        //Operators
+        clear.setOnClickListener { appendVal("", true) }
+        startBracket.setOnClickListener { appendVal(" ( ", false) }
+        closeBracket.setOnClickListener { appendVal(" ) ", false) }
+        actionDivide.setOnClickListener { appendVal(" / ", false) }
+        actionMultiply.setOnClickListener { appendVal(" * ", false) }
+        actionMinus.setOnClickListener { appendVal(" - ", false) }
+        actionAdd.setOnClickListener { appendVal(" + ", false) }
+
+        /*
+        actionBack.setOnClickListener {
+            val expression = placeholder.text.toString()
+            if (expression.isNotEmpty()) {
+                placeholder.text = expression.substring(0, expression.length - 1)
             }
-            "AC" -> {resultTV.setText(null)}
-            "=" -> {
-                var evaluator = DoubleEvaluator()
-                var expression = resultTV.text.replace(Regex("×"), "*")
-                var result = evaluator.evaluate(expression)
-                resultTV.setText(result.toString())
+
+
+        }
+
+         */
+
+        actionEquals.setOnClickListener {
+            try {
+                val expression = ExpressionBuilder(placeholder.text.toString()).build()
+                val result = expression.evaluate()
+                val longResult = result.toLong()
+                if (result == longResult.toDouble()) {
+                    Toast.makeText(this, "Double", Toast.LENGTH_SHORT).show()
+                    answer.text = longResult.toString()
+                } else
+                    answer.text = result.toString()
+
+            } catch (e: Exception) {
+                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show();
+
+                Log.d("EXCEPTION", "Message: ${e.message}")
             }
-            else -> {
-                var toAppendString = tv.text.toString()
-                if(isOperator(toAppendString[0]) && isOperator(oldText[oldText.length - 1])){
-                    oldText = oldText.substring(0, oldText.length - 1)
-                }
-                var newText = oldText + toAppendString
-                resultTV.setText(newText)
-            }
+
+        }
+
+
+    }
+
+    fun appendVal(string: String, isClear: Boolean) {
+        if (isClear) {
+            placeholder.text = ""
+            answer.text = ""
+//            placeholder.append(string)
+        } else {
+            placeholder.append(string)
         }
     }
-    private fun isOperator(c: Char): Boolean {
-        when(c){
-            'x', '*', '×', '/', '+', '-' -> { return true}
-            else -> return false
-        }
-    }
+
 }
